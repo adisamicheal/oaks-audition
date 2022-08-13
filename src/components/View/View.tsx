cc94a5d3c8ba049b1dc46179ec3a6a755b0b0641
adisamicheal <adisamicheal20@gmail.com>
2022-08-13 19:26:39 +0100

    set up app interface

---
+++ src/components/View/View.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './View.module.css'
+
+const View = () => {
+    return (
+        <div className={styles.container}>
+            <h3>This is the app interface</h3>
+        </div>
+    )
+}
+
+export default View
